0f1b286715d500edd3edd39515015d314385894b
1essy <lestia495@yahoo.co.jp>
2022-04-18 02:27:04 +0000

    コントローラー追加

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,19 @@
+class OrdersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def log
+  end
+
+  def create
+  end
+
+  def complete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
   
- 
-    # 顧客用
-  # URL /customers/sign_in ...
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-    # 管理者用
-  # URL /admin/sign_in ...
+  
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-   namespace :public do
-    get 'homes/about'
-  end
-  root :to => "public/homes#top"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  #admin
   namespace :admin do
     resources :items
   end
+  
+  #public
+  root :to => "public/homes#top"
+  namespace :public do
+    get 'homes/about'
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
