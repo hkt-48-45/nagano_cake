cafd4995bf13a71a9617efc80abe723c4816d27a
EC2 Default User <ec2-user@ip-172-31-19-167.ec2.internal>
2022-04-20 08:21:44 +0000

    [会員側]商品・カート機能

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,10 @@ Rails.application.routes.draw do
   root :to => "public/homes#top"
   get 'homes/about' => "public/homes#about"
   get 'homes/top' => "public/homes#top"
+  delete "cart_items/destroy_all" => "cart_items#destroy_all"
   resources :customers
+  resources :items, only: [:index, :show]
+  resources :cart_items, only: [:index, :create, :update, :destroy]
   #get "customers/:id/withdraw_confirm" => "customers#withdraw_confirm", as: "withdraw_confirm"
   #path "customers/:id/withdraw" => "customers#withdraw",as: "withdraw"
 
