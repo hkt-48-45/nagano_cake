812734c39ccd17c8088301575a048116a7762fa2
RT <u7def37decdwd@gmail.com>
2022-04-24 02:14:20 +0000

    注文機能作成完了

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,15 @@
 class Order < ApplicationRecord
 
   has_many :order_details, dependent: :destroy
-  has_many :items, through: :order_details
+  has_many :items
   belongs_to :customer
-  
-  
+
+
   enum status: {nyuukinmati: 0,nyuukinnkakuninn: 1,seisakutyuu: 2,hassouzyunbityuu: 3, hassouzumi: 4}
   enum payment_method: {credit_card: 0, transfer: 1 }
+
+  def order_display
+    "〒" + post_number + " " + address + " " + name
+  end
 
 end
